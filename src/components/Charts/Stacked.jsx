79387dac18566a797dcd039f54b18dbae6568f87
adetayo <adetayoabiodun4@gmail.com>
2024-08-10 11:33:53 +0100

    chartcomponent

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+const Stacked = ({width, height}) => {
+  return (
+    <ChartComponent
+    width={width}
+    height={height}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
