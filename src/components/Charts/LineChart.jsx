558655d8c9152d662a933c35087db867c201a8bc
adetayo <adetayoabiodun4@gmail.com>
2024-10-11 09:39:19 +0100

    Linechart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  LineSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  return (
+    <ChartComponent>
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
