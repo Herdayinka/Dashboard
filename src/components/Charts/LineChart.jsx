5fef6601df3f40ab7ff316e33222159c7406eee1
adetayo <adetayoabiodun4@gmail.com>
2024-10-14 23:04:20 +0100

    Area-chart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -16,13 +16,20 @@ import {
   LinePrimaryXAxis,
 } from "../../data/dummy";
 
+import { useStateContext } from "../../contexts/ContextProvider";
+
 const LineChart = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
       id="line-chart"
       height="420px"
       primaryXAxis={LinePrimaryXAxis}
       primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
     >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
